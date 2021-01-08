564fc298a1e65fc12ef0e1e67cdf9e1c804a120c
endroni <rubao.rcs@gmail.com>
2021-01-08 11:14:54 -0300

    redirecionamento dinamico das paginas php

--- index.php
+++ index.php
@@ -35,19 +35,19 @@
                 <h1 class="oculto">Mais sobre HTML</h1>
                 <ul class="menu">
                     <li>
-                        <a href="#">Início</a>
+                        <a href="index.php?prm=home">Início</a>
                     </li>
                     <li>
-                        <a href="#">Audio</a>
+                        <a href="index.php?prm=audio">Audio</a>
                     </li>
                     <li>
-                        <a href="#">Vídeo</a>
+                        <a href="index.php?prm=video">Vídeo</a>
                     </li>
                     <li>
-                        <a href="#">Formulário</a>
+                        <a href="index.php?prm=formulario">Formulário</a>
                     </li>
                     <li>
-                        <a href="#">Sobre</a>
+                        <a href="index.php?prm=sobre">Sobre</a>
                     </li>
                 </ul>
             </nav>
@@ -58,161 +58,20 @@
 
     <div class="clear"></div>
 
-    <section class="slide background-fixed-1 color-white text-center">
-        
-            <h1 class="titulo-slide">Curso Web Developer</h1>
-            <p>Desenvolvendo Softwares profissionais para a Web</p>    
-             
-    </section> <!-- Finaliza o slide -->
+    <!-- Conteúdo dinâmico -->
+    <?php
 
-    <section class="detalhes-curso text-center padding-30">
-        <div class="container">
-            <div class="padding-30">
-                <h1 class="titulo">Vamos aprender a programar</h1>
-                <p>Bora lá galera!</p>
-            </div>        
-
-            <div class="row">
-                <article class=" col-md-3 col-sm-6 col-xs-12">
-                    <div class="detalhes">
-                        <img src="./imgs/icones/curso-pratico-especializati.png" alt="HTML5 Curso prático" title="Curso HTML5">
-                        <h1 class="titulo-detalhe">Prático</h1>
-                    </div>
-                </article>
-
-                <article class=" col-md-3 col-sm-6 col-xs-12">
-                    <div class="detalhes">
-                    <img src="./imgs/icones/curso-online-especializati.png" alt="HTML5 Curso rápido" title="Curso HTML5">
-                    <h1 class="titulo-detalhe">Rápido</h1>
-                </div>
-                </article>
-
-                <article class=" col-md-3 col-sm-6 col-xs-12">
-                    <div class="detalhes">
-                    <img src="./imgs/icones/projetos-reais-especializati.png" alt="HTML5 Curso divertido" title="Curso HTML5">
-                    <h1 class="titulo-detalhe">Divertido</h1>
-                </div>
-                </article>
-
-                <article class=" col-md-3 col-sm-6 col-xs-12">
-                    <div class="detalhes">
-                    <img src="./imgs/icones/certificado-digital-especializati.png" alt="HTML5 Curso profissional" title="Curso HTML5">
-                    <h1 class="titulo-detalhe">Profissional</h1>
-                </div>
-                </article>
-            </div>
-
-        </div>
-    </section> <!-- Detalhes-curso -->
-
-
-    <section class="propaganda text-center padding-60 bg-blue-one color-white">
-        <div class="container col-md-12 padding-10">
-            <h1 class="titulo-propaganda">
-                Aprendendo a desenvolver de maneira profissional, sites modernos e avançados. 
-            </h1>            
-        </div>
-
-        <div class="container">            
-            <a class="btn-saiba-mais2" href="">Saiba Mais</a>
-        </div>
-    </section> <!-- Final Propaganda -->
-
-
-    <section class="cursos text-center padding-30">
-        <div class="container">
-            <h1 class="titulo padding-30">Conteúdo <mark>Curso Web Developer</mark></h1>
-
-            <div class="row">
-
-                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
-                    <img src="./imgs/conteudo-cursos/treinamento-php-especializati.png" alt="Curso PHP" title="Curso PHP" class="img-curso">
-                    <h1 class="titulo-curso">
-                        Curso de PHP
-                    </h1>
-                    <p class="descricao">
-                        Aprenda PHP 
-                    </p>
-                </article>   
-
-                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
-                    <img src="./imgs/conteudo-cursos/treinamento-javascript-especializati.png" alt="Curso JavaScript" title="Curso PHP" class="img-curso">
-                    <h1 class="titulo-curso">
-                        Curso de JavaScript
-                    </h1>
-                    <p class="descricao">
-                        Aprenda JavaScript 
-                    </p>
-                </article> 
-
-                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
-                    <img src="./imgs/conteudo-cursos/treinamento-jquery-especializati.png" alt="Curso jQuery" title="Curso jQuery" class="img-curso">
-                    <h1 class="titulo-curso">
-                        Curso de jQuery
-                    </h1>
-                    <p class="descricao">
-                        Aprenda jQuery 
-                    </p>
-                </article> 
-
-                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
-                    <img src="./imgs/conteudo-cursos/treinamento-ajax-especializati.png" alt="Curso Ajax" title="Curso Ajax" class="img-curso">
-                    <h1 class="titulo-curso">
-                        Curso de Ajax
-                    </h1>
-                    <p class="descricao">
-                        Aprenda Ajax 
-                    </p>
-                </article>                
-
-                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
-                    <img src="./imgs/conteudo-cursos/treinamento-seo-especializati.png" alt="Curso SEO" title="Curso SEO" class="img-curso">
-                    <h1 class="titulo-curso">
-                        Curso de SEO
-                    </h1>
-                    <p class="descricao">
-                        Aprenda SEO (Google, Yahoo, Bing)
-                    </p>
-                </article>
-
-                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
-                    <img src="./imgs/conteudo-cursos/desenvolvimento-sistemas-especializati.png" alt="Curso Desenvolvimento de Sistemas" title="Curso Desenvolvimento de Sistemas" class="img-curso">
-                    <h1 class="titulo-curso">
-                        Curso de Desenvolvimento de Sistemas
-                    </h1>
-                    <p class="descricao">
-                        Aprenda Desenvolvimento de Sistemas
-                    </p>
-                </article>
-
-            </div>
-        </div>
-    </section> <!-- Fim cursos -->
-
-    <section class="propaganda-dois padding-70 background-fixed-2 color-white text-center">
-        <h1 class="titulo padding-20">Aprendendo a aprender, crescendo juntos</h1>
-        <a class="btn-saiba-mais3">Saiba Mais</a>
-    </section> <!-- Final propaganda 2 -->
-
-    <section class="mais-detalhes">
-        <div class="container">
-            <div class="row">
-                <h1 class="oculto">Curso abrangente</h1>
-
-                <article class="col-md-6">
-                    <h1 class="titulo-detalhe">Sólido e abrangente</h1>
-
-                    <p class="descricao">
-                        Crescimento contínuo!
-                    </p>
-                </article>
-
-                <div class="col-md-6 text-center">
-                    <img src="./imgs/desenvolvimento-responsivo.png" alt="Desenvolvendo sites responsivos" title="Desenvolvendo sites responsivos" class="img-designer-responsivo">
-                </div>
-            </div>
-        </div>
-    </section> <!-- Final Mais Detalhes -->
+        if( isset($_GET['prm']) ){
+            // Uma camada de segurança para evitar php injection - Quando alguém cria um php ao não encontrar a página
+            if ( file_exists("./pags/{$_GET['prm']}.php") )
+                include("./pags/{$_GET['prm']}.php");
+            else
+                include('./pags/404.php');
+        }else{
+            include('./pags/home.php');
+        }
+    ?>
+    <!-- Fim redirecionamento dinâmico PHP -->
 
     <footer class="footer color-white">
         <h1 class="oculto">Detalhes sobre o Curso Web Developer</h1>
---
+++ pags/home.php
@@ -0,0 +1,155 @@
+    <section class="slide background-fixed-1 color-white text-center">
+        
+            <h1 class="titulo-slide">Curso Web Developer</h1>
+            <p>Desenvolvendo Softwares profissionais para a Web</p>    
+             
+    </section> <!-- Finaliza o slide -->
+
+    <section class="detalhes-curso text-center padding-30">
+        <div class="container">
+            <div class="padding-30">
+                <h1 class="titulo">Vamos aprender a programar</h1>
+                <p>Bora lá galera!</p>
+            </div>        
+
+            <div class="row">
+                <article class=" col-md-3 col-sm-6 col-xs-12">
+                    <div class="detalhes">
+                        <img src="./imgs/icones/curso-pratico-especializati.png" alt="HTML5 Curso prático" title="Curso HTML5">
+                        <h1 class="titulo-detalhe">Prático</h1>
+                    </div>
+                </article>
+
+                <article class=" col-md-3 col-sm-6 col-xs-12">
+                    <div class="detalhes">
+                    <img src="./imgs/icones/curso-online-especializati.png" alt="HTML5 Curso rápido" title="Curso HTML5">
+                    <h1 class="titulo-detalhe">Rápido</h1>
+                </div>
+                </article>
+
+                <article class=" col-md-3 col-sm-6 col-xs-12">
+                    <div class="detalhes">
+                    <img src="./imgs/icones/projetos-reais-especializati.png" alt="HTML5 Curso divertido" title="Curso HTML5">
+                    <h1 class="titulo-detalhe">Divertido</h1>
+                </div>
+                </article>
+
+                <article class=" col-md-3 col-sm-6 col-xs-12">
+                    <div class="detalhes">
+                    <img src="./imgs/icones/certificado-digital-especializati.png" alt="HTML5 Curso profissional" title="Curso HTML5">
+                    <h1 class="titulo-detalhe">Profissional</h1>
+                </div>
+                </article>
+            </div>
+
+        </div>
+    </section> <!-- Detalhes-curso -->
+
+
+    <section class="propaganda text-center padding-60 bg-blue-one color-white">
+        <div class="container col-md-12 padding-10">
+            <h1 class="titulo-propaganda">
+                Aprendendo a desenvolver de maneira profissional, sites modernos e avançados. 
+            </h1>            
+        </div>
+
+        <div class="container">            
+            <a class="btn-saiba-mais2" href="">Saiba Mais</a>
+        </div>
+    </section> <!-- Final Propaganda -->
+
+
+    <section class="cursos text-center padding-30">
+        <div class="container">
+            <h1 class="titulo padding-30">Conteúdo <mark>Curso Web Developer</mark></h1>
+
+            <div class="row">
+
+                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
+                    <img src="./imgs/conteudo-cursos/treinamento-php-especializati.png" alt="Curso PHP" title="Curso PHP" class="img-curso">
+                    <h1 class="titulo-curso">
+                        Curso de PHP
+                    </h1>
+                    <p class="descricao">
+                        Aprenda PHP 
+                    </p>
+                </article>   
+
+                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
+                    <img src="./imgs/conteudo-cursos/treinamento-javascript-especializati.png" alt="Curso JavaScript" title="Curso PHP" class="img-curso">
+                    <h1 class="titulo-curso">
+                        Curso de JavaScript
+                    </h1>
+                    <p class="descricao">
+                        Aprenda JavaScript 
+                    </p>
+                </article> 
+
+                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
+                    <img src="./imgs/conteudo-cursos/treinamento-jquery-especializati.png" alt="Curso jQuery" title="Curso jQuery" class="img-curso">
+                    <h1 class="titulo-curso">
+                        Curso de jQuery
+                    </h1>
+                    <p class="descricao">
+                        Aprenda jQuery 
+                    </p>
+                </article> 
+
+                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
+                    <img src="./imgs/conteudo-cursos/treinamento-ajax-especializati.png" alt="Curso Ajax" title="Curso Ajax" class="img-curso">
+                    <h1 class="titulo-curso">
+                        Curso de Ajax
+                    </h1>
+                    <p class="descricao">
+                        Aprenda Ajax 
+                    </p>
+                </article>                
+
+                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
+                    <img src="./imgs/conteudo-cursos/treinamento-seo-especializati.png" alt="Curso SEO" title="Curso SEO" class="img-curso">
+                    <h1 class="titulo-curso">
+                        Curso de SEO
+                    </h1>
+                    <p class="descricao">
+                        Aprenda SEO (Google, Yahoo, Bing)
+                    </p>
+                </article>
+
+                <article class="col-md-3 col-sm-6 col-xs-12 box-detalhes-curso">
+                    <img src="./imgs/conteudo-cursos/desenvolvimento-sistemas-especializati.png" alt="Curso Desenvolvimento de Sistemas" title="Curso Desenvolvimento de Sistemas" class="img-curso">
+                    <h1 class="titulo-curso">
+                        Curso de Desenvolvimento de Sistemas
+                    </h1>
+                    <p class="descricao">
+                        Aprenda Desenvolvimento de Sistemas
+                    </p>
+                </article>
+
+            </div>
+        </div>
+    </section> <!-- Fim cursos -->
+
+    <section class="propaganda-dois padding-70 background-fixed-2 color-white text-center">
+        <h1 class="titulo padding-20">Aprendendo a aprender, crescendo juntos</h1>
+        <a class="btn-saiba-mais3">Saiba Mais</a>
+    </section> <!-- Final propaganda 2 -->
+
+    <section class="mais-detalhes">
+        <div class="container">
+            <div class="row">
+                <h1 class="oculto">Curso abrangente</h1>
+
+                <article class="col-md-6">
+                    <h1 class="titulo-detalhe">Sólido e abrangente</h1>
+
+                    <p class="descricao">
+                        Crescimento contínuo!
+                    </p>
+                </article>
+
+                <div class="col-md-6 text-center">
+                    <img src="./imgs/desenvolvimento-responsivo.png" alt="Desenvolvendo sites responsivos" title="Desenvolvendo sites responsivos" class="img-designer-responsivo">
+                </div>
+            </div>
+        </div>
+    </section> <!-- Final Mais Detalhes -->
