1ceefecc525b12f1f109a9cf8ca9c72a8c9de3ca
endroni <rubao.rcs@gmail.com>
2021-01-11 14:51:30 -0300

    pagina finalizada por hora

--- pags/sobre.php
+++ pags/sobre.php
@@ -1,1 +1,28 @@
-Sobre nós!
+<?php
+	$namePage = 'Sobre nós';
+	include('pags/sem-slide.php');
+?>
+
+
+
+<section class="audio padding-30">
+	<div class="container text-center">
+		<h1 class="titulo padding-10">
+			Sobre nós!
+		</h1>
+
+		<div class="row">
+			<div class="col-md-6 text-justify">
+				Vamos falar sobre nossos sonhos!
+				<ul>
+					<li>Cursos</li>
+					<li>Certificações</li>
+				</ul>
+			</div>
+			
+			<div class="col-md-6 text-justify">
+				<img src="./imgs/laptop-1209008_1280_red.jpg" alt="laptop e outros itens de escritório" title="Escritório">
+			</div>
+		</div>
+	</div>
+</section>
