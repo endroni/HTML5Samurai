0626634a851d8694f8de297a8bcb6a3bf86b970d
endroni <rubao.rcs@gmail.com>
2021-01-11 10:10:10 -0300

    include audio

--- pags/audios.php
+++ pags/audios.php
@@ -3,8 +3,14 @@
 	include('pags/sem-slide.php');
 ?>
 
+
+
 <section class="audio">
 	<div class="container">
-		Audio
+		<h1 class="titulo">
+			Trabalhando com audio no HTML5
+		</h1>
+
+		<audio src="./files/Survivor-EyeOfTheTiger.mp3" controls loop></audio>
 	</div>
 </section>
